d43d4864b15ef49296b148023e2d996be281b6d2
Mu3ssam <94981148+3ssaamm@users.noreply.github.com>
2024-04-24 22:00:23 +0200

    Initial commit

---
+++ Assets/Scripts/Controllers.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllers : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+        // Get the input from the keyboard.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Rotate the object around the x-axis.
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * horizontalInput);
+
+        // Move the object forward, backward, left, or right in world space.
+        transform.Translate(transform.forward * speed * Time.deltaTime * verticalInput, Space.World);
+        //transform.Translate(transform.right * speed * Time.deltaTime * horizontalInput, Space.World);
+    }
+}
+
